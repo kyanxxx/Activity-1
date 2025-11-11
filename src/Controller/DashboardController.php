2edc894ef18c0d624b867c65365f44d9699ead9e
kyanxxx <luzanokhianharvey@gmail.com>
2025-11-11 12:05:55 +0800

    Updates for Activity-2

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        return new Response("Welcome to the dashboard! (Protected Area)");
+         return $this->render('base.html.twig', [
+
+         ]);
     }
 }
